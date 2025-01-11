9c36f82428cc898f2dbf86ee8a6985b474c4141b
brandistone <brandymabeya@gmail.com>
2025-01-11 20:51:16 +0300

    Added a new page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,5 @@
 import Image from "next/image";
-import Link from "next/link";
+
 
 
 
@@ -65,7 +65,7 @@ export default function Home() {
 
       
 
-      <Link href="/page">Go to Layout</Link> {/* Corrected Link */}
+      
      
 
     
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -98,7 +98,7 @@ const Navbar = () => {
                             Log in
                         </a>
                         <a
-                            href="/sign in"
+                            href="/signup"
                             className="flex items-center justify-center gap-x-1 py-2 px-4 text-white font-medium bg-gray-800 hover:bg-gray-700 active:bg-gray-900 rounded-full md:inline-flex"
                         >
                             Sign in
